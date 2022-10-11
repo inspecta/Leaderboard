b7852e78468c9ca983cf49cc71051af1eb94e4e6
derrick <djmulinde17@gmail.com>
2022-10-12 01:55:54 +0300

    Import project modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css'
+import Score from './modules/Score'
+import displayData from './modules/displayData'
+import LeadershipData from './modules/LeadershipData'
+import refreshDisplay from './modules/refreshDisplay'
+
+const form = document.getElementById('form')
+const name = document.getElementById('name')
+const score = document.getElementById('score')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const newScore = new Score(name.value, score.value);
+  LeadershipData.push(newScore);
+})
+
+refreshDisplay();
+displayData(LeadershipData);
