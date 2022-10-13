dd4c4eaa7fa20da950d5dfa8f0efb55fcae5b5ab
Derrick <djmulinde@gmail.com>
2022-10-13 09:38:14 +0300

    Add fetch data and display module

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -1,15 +1,29 @@
+const id = localStorage.getItem('game');
 const display = document.querySelector('.names-scores');
 
-const displayData = (LeadershipData) => {
-  if (!LeadershipData.length) display.innerHTML = '<p class=\'details\'>No scores available!</p>';
+const displayData = (leaderboard) => {
+  if (leaderboard) {
+    if (!leaderboard.length) display.innerHTML = '<p class=\'details\'>No scores available!</p>';
 
-  LeadershipData.forEach((data) => {
-    const nameHolder = document.createElement('p');
-    nameHolder.classList.add('details');
-    nameHolder.id = 'details';
-    display.appendChild(nameHolder);
-    nameHolder.innerHTML = `${data.name} : ${data.score}`;
-  });
+    leaderboard.forEach((data) => {
+      const nameHolder = document.createElement('p');
+      nameHolder.classList.add('details');
+      nameHolder.id = 'details';
+      display.appendChild(nameHolder);
+      nameHolder.innerHTML = `${data.user} : ${data.score}`;
+    });
+  }
 };
+
+const fetchScoresFromApi = async () => {
+  if (id) {
+    const fetchUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/`;
+    const response = await fetch(fetchUrl);
+    const fetchedData = await response.json();
+    displayData(fetchedData.result);
+  }
+};
+
+fetchScoresFromApi();
 
 export default displayData;
